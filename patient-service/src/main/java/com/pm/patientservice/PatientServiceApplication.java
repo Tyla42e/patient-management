aa266af74ab366f69166174848c296e46f6c2eaa
Tyla42e <tyla42e@hotmail.com>
2025-05-08 14:51:52 +1000

    Added Service Discovery

--- patient-service/src/main/java/com/pm/patientservice/PatientServiceApplication.java
+++ patient-service/src/main/java/com/pm/patientservice/PatientServiceApplication.java
@@ -3,6 +3,7 @@ package com.pm.patientservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class PatientServiceApplication {
 
